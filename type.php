f0ced62251e8868f38673ff9bf82ce76043b6866
Tran Vu <vutt94@gmail.com>
2023-06-11 13:56:41 +0700

    update type management

--- type.php
+++ type.php
@@ -6,43 +6,45 @@ if (!isset($_SESSION['admin'])) {
     exit();
 }
 
+include 'connection.php';
+
 // Handle CRUD operations for types
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Add type
-    if (isset($_POST['add'])) {
-        $name = $_POST['name'];
-        $type = $_POST['type'];
-        $count = $_POST['count'];
-        $price = $_POST['price'];
-        $note = $_POST['note'];
-
-        $query = "INSERT INTO types VALUES ($name, $type, $count, $price,$note)";
-		$result = mysqli_query($db, $query) or die(mysqli_error($db));
-    }
-
     // Edit or Delete type
-    if (isset($_POST['action']) && isset($_POST['type_id'])) {
+    if (isset($_POST['action'])) {
         $action = $_POST['action'];
-        $type_id = $_POST['type_id'];
 
-        if ($action === 'edit') {
-            $updated_type = $_POST['updated_type'];
-            // Update the type in the database
-            // Add your database code here
-        } elseif ($action === 'delete') {
-            // Delete the type from the database
-            // Add your database code here
+        if ($action === 'add') {
+            $name = $_POST['name'];
+            $type = $_POST['type'];
+            $count = $_POST['count'];
+            $price = $_POST['price'];
+            $note = $_POST['note'];
+
+            $query = "INSERT INTO types (name, type, count, price, note) VALUES ('$name', '$type', '$count', '$price','$note')";
+        } elseif ($action === 'edit') {
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $type = $_POST['type'];
+            $count = $_POST['count'];
+            $price = $_POST['price'];
+            $note = $_POST['note'];
+
+            $query = "UPDATE types SET name = '$name', type = '$type', count = '$count', price = '$price', note = '$note' WHERE id='$id'";
+        } else{
+            $id = $_POST['id'];
+            $query = "DELETE FROM types WHERE id='$id'";
         }
+
+        $result = mysqli_query($db, $query) or die(mysqli_error($db));
+
     }
 }
+?>
 
-// Fetch types from the database
-// Add your database code here to retrieve the types
-$types = [
-    ['id' => 1, 'type' => 'Type 1'],
-    ['id' => 2, 'type' => 'Type 2'],
-    ['id' => 3, 'type' => 'Type 3'],
-];
+<?php
+    $query = "SELECT * FROM types";
+    $result = mysqli_query($db, $query) or die(mysqli_error($db));
 ?>
 
 <?php include 'head.php'; ?>
@@ -86,7 +88,47 @@ $types = [
                         <label for="inputCount">Ghi chú</label>
                     </div>
                     <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                        <button type="submit" name="add" class="btn btn-primary">Thêm</a>
+                        <button type="submit" name="action" value="add" class="btn btn-primary">Thêm</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div id="form-edit" class="card mb-4" style="display: none">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                    Sửa thông tin sảnh
+            </div>
+            <div class="card-body">
+                <form method="POST" action="">
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputNameUpdate" type="text" placeholder="Sảnh A"  name="name" required />
+                        <label for="inputNameUpdate">Tên sảnh</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <select class="form-control" id="inputType" name="type" required>
+                            <option value="A" data-price="1000000">A</option>
+                            <option value="B" data-price="1100000">B</option>
+                            <option value="C" data-price="1200000">C</option>
+                            <option value="D" data-price="1400000">D</option>
+                            <option value="E" data-price="1600000">E</option>
+                        </select>
+                        <label for="inputType">Loại sảnh</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputCountUpdate" type="number" placeholder="30"  name="count" required />
+                        <label for="inputCountUpdate">Số lượng bàn tối đa</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputPriceUpdate" type="price" placeholder="1000000"  name="price" value="1000000" required readonly />
+                        <label for="inputPriceUpdate">Đơn giá bàn tối thiểu</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputNoteUpdate" type="textArea" placeholder="Chỉ nhận 30 bàn trở lên"  name="note" />
+                        <label for="inputCountUpdate">Ghi chú</label>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                        <input id="inputIdUpdate" type="hidden" name="id" />
+                        <button type="submit" name="action" value="edit" class="btn btn-primary">Cập nhật</button>
                     </div>
                 </form>
             </div>
@@ -97,26 +139,41 @@ $types = [
                     Danh sách sảnh
             </div>
             <div class="card-body">
-            <table>
+            <table class="table">
                     <tr>
-                        <th>Type ID</th>
+                        <th>ID</th>
+                        <th>Name</th>
                         <th>Type</th>
+                        <th>Count</th>
+                        <th>Price</th>
+                        <th>Note</th>
                         <th>Action</th>
                     </tr>
-                    <?php foreach ($types as $type) : ?>
+                    <?php while($row = mysqli_fetch_array($result)): ?>
                         <tr>
-                            <td><?= $type['id']; ?></td>
-                            <td><?= $type['type']; ?></td>
+                            <td><?= $row['id']; ?></td>
+                            <td><?= $row['name']; ?></td>
+                            <td><?= $row['type']; ?></td>
+                            <td><?= $row['count']; ?></td>
+                            <td><?= $row['price']; ?></td>
+                            <td><?= $row['note']; ?></td>
                             <td>
                                 <form method="POST" action="">
-                                    <input type="hidden" name="type_id" value="<?= $type['id']; ?>">
-                                    <input type="text" name="updated_type" placeholder="Updated Type" required>
-                                    <button type="submit" name="action" value="edit">Update</button>
-                                    <button type="submit" name="action" value="delete">Delete</button>
+                                    <input type="hidden" name="id" value="<?= $row['id']; ?>">
+                                    <button 
+                                        class="btn btn-primary btn-edit" 
+                                        data-id="<?= $row['id']; ?>"
+                                        data-name="<?= $row['name']; ?>"
+                                        data-type="<?= $row['type']; ?>"
+                                        data-count="<?= $row['count']; ?>"
+                                        data-price="<?= $row['price']; ?>"
+                                        data-note="<?= $row['note']; ?>"
+                                        type="button">Sửa</button>
+                                    <button class="btn btn-danger" type="submit" name="action" value="delete">Xoá</button>
                                 </form>
                             </td>
                         </tr>
-                    <?php endforeach; ?>
+                    <?php endwhile; ?>
                 </table>
             </div>               
         </div>
@@ -129,6 +186,17 @@ $types = [
         $('#inputType').on('change', function() {
             var price = $(this).find(":selected").data('price');
             $('#inputPrice').val(price);
+        });
+
+        $(".btn-edit").click(function(){
+            $("#inputIdUpdate").val($(this).data('id'));
+            $("#inputNameUpdate").val($(this).data('name'));
+            $("#inputTypeUpdate").val($(this).data('type'));
+            $("#inputCountUpdate").val($(this).data('count'));
+            $("#inputPriceUpdate").val($(this).data('price'));
+            $("#inputNoteUpdate").val($(this).data('note'));
+
+            $("#form-edit").css('display','block');
         });
     });
 </script>

b81e85a8bfe9aa4433892ef9f83512acd497e659
Parrmas <115953300+Parrmas@users.noreply.github.com>
2023-06-15 15:51:19 +0700

    Dishes + shifts + service management fuction implemented.

--- service.php
+++ service.php
@@ -6,87 +6,149 @@ if (!isset($_SESSION['admin'])) {
     exit();
 }
 
+include 'connection.php';
+
 // Handle CRUD operations for services
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Add service
-    if (isset($_POST['add_service'])) {
-        $service_name = $_POST['service_name'];
-        $description = $_POST['description'];
-        $price = $_POST['price'];
-        // Insert the new service into the database
-        // Add your database code here
-    }
-
     // Edit or Delete service
-    if (isset($_POST['action']) && isset($_POST['service_id'])) {
+    if (isset($_POST['action'])) {
         $action = $_POST['action'];
-        $service_id = $_POST['service_id'];
 
-        if ($action === 'edit') {
-            $updated_service_name = $_POST['updated_service_name'];
-            $updated_description = $_POST['updated_description'];
-            $updated_price = $_POST['updated_price'];
-            // Update the service in the database
-            // Add your database code here
-        } elseif ($action === 'delete') {
-            // Delete the service from the database
-            // Add your database code here
+        if ($action === 'add') {
+            $serviceName = $_POST['service_name'];
+            $description = $_POST['description'];
+            $price = $_POST['price'];
+
+            $query = "INSERT INTO services (service_name, description, price) VALUES ('$serviceName', '$description', '$price')";
+        } elseif ($action === 'edit') {
+            $id = $_POST['id'];
+            $serviceName = $_POST['service_name'];
+            $description = $_POST['description'];
+            $price = $_POST['price'];
+
+            $query = "UPDATE services SET service_name = '$serviceName', description = '$description', price = '$price' WHERE id='$id'";
+        } else {
+            $id = $_POST['id'];
+            $query = "DELETE FROM services WHERE id='$id'";
         }
+
+        $result = mysqli_query($db, $query) or die(mysqli_error($db));
     }
 }
 
-// Fetch services from the database
-// Add your database code here to retrieve the services
-$services = [
-    ['id' => 1, 'service_name' => 'Service 1', 'description' => 'Description 1', 'price' => 10.99],
-    ['id' => 2, 'service_name' => 'Service 2', 'description' => 'Description 2', 'price' => 19.99],
-    ['id' => 3, 'service_name' => 'Service 3', 'description' => 'Description 3', 'price' => 14.99],
-];
+$query = "SELECT * FROM services";
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Service Management</title>
-</head>
-<body>
-    <h1>Service Management</h1>
+<?php include 'head.php'; ?>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Quản lý tiệc cưới</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Quản lý dịch vụ</li>
+        </ol>
+        <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Thêm dịch vụ mới
+            </div>
+            <div class="card-body">
+                <form method="POST" action="">
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputServiceName" type="text" placeholder="Service A"  name="service_name" required />
+                        <label for="inputServiceName">Tên dịch vụ</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" id="inputDescription" placeholder="Service Description" name="description"></textarea>
+                        <label for="inputDescription">Mô tả</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputPrice" type="number" placeholder="200000" name="price" required/>
+                        <label for="inputPrice">Đơn giá</label>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                        <button type="submit" name="action" value="add" class="btn btn-primary">Thêm</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div id="form-edit" class="card mb-4" style="display: none">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Sửa thông tin dịch vụ
+            </div>
+            <div class="card-body">
+                <form method="POST" action="">
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputServiceNameUpdate" type="text" placeholder="Service A"  name="service_name" required />
+                        <label for="inputServiceNameUpdate">Tên dịch vụ</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" id="inputDescriptionUpdate" placeholder="Service Description" name="description"></textarea>
+                        <label for="inputDescriptionUpdate">Mô tả</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputPriceUpdate" type="number" placeholder="200000" name="price" required/>
+                        <label for="inputPriceUpdate">Đơn giá</label>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                        <input id="inputIdUpdate" type="hidden" name="id"/>
+                        <button type="submit" name="action" value="edit" class="btn btn-primary">Cập nhật</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Danh sách dịch vụ
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>Tên dịch vụ</th>
+                        <th>Mô tả</th>
+                        <th>Đơn giá</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php while ($row = mysqli_fetch_array($result)): ?>
+                        <tr>
+                            <td><?= $row['id']; ?></td>
+                            <td><?= $row['service_name']; ?></td>
+                            <td><?= $row['description']; ?></td>
+                            <td><?= number_format($row['price']); ?></td>
+                            <td>
+                                <form method="POST" action="">
+                                    <input type="hidden" name="id" value="<?= $row['id']; ?>">
+                                    <button
+                                            class="btn btn-primary btn-edit"
+                                            data-id="<?= $row['id']; ?>"
+                                            data-service-name="<?= $row['service_name']; ?>"
+                                            data-description="<?= $row['description']; ?>"
+                                            data-price="<?= number_format($row['price']); ?>"
+                                            type="button">Sửa</button>
+                                    <button class="btn btn-danger" type="submit" name="action" value="delete">Xóa</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include 'foot.php'; ?>
 
-    <h2>Add Service</h2>
-    <form method="POST" action="">
-        <input type="text" name="service_name" placeholder="Service Name" required>
-        <input type="text" name="description" placeholder="Description" required>
-        <input type="number" name="price" placeholder="Price" required>
-        <button type="submit" name="add_service">Add</button>
-    </form>
+<script>
+    $(function(){
+        $(".btn-edit").click(function(){
+            $("#inputIdUpdate").val($(this).data('id'));
+            $("#inputServiceNameUpdate").val($(this).data('service-name'));
+            $("#inputDescriptionUpdate").val($(this).data('description'));
+            $("#inputPriceUpdate").val($(this).data('price'));
 
-    <h2>Edit/Delete Service</h2>
-    <table>
-        <tr>
-            <th>Service ID</th>
-            <th>Service Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Action</th>
-        </tr>
-        <?php foreach ($services as $service) : ?>
-            <tr>
-                <td><?= $service['id']; ?></td>
-                <td><?= $service['service_name']; ?></td>
-                <td><?= $service['description']; ?></td>
-                <td><?= $service['price']; ?></td>
-                <td>
-                    <form method="POST" action="">
-                        <input type="hidden" name="service_id" value="<?= $service['id']; ?>">
-                        <input type="text" name="updated_service_name" placeholder="Updated Service Name" required>
-                        <input type="text" name="updated_description" placeholder="Updated Description" required>
-                        <input type="number" name="updated_price" placeholder="Updated Price" required>
-                        <button type="submit" name="action" value="edit">Update</button>
-                        <button type="submit" name="action" value="delete">Delete</button>
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</body>
-</html>
+            $("#form-edit").css('display','block');
+        });
+    });
+</script>
--- shift.php
+++ shift.php
@@ -6,87 +6,149 @@ if (!isset($_SESSION['admin'])) {
     exit();
 }
 
+include 'connection.php';
+
 // Handle CRUD operations for shifts
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Add shift
-    if (isset($_POST['add_shift'])) {
-        $shift_name = $_POST['shift_name'];
-        $start_time = $_POST['start_time'];
-        $end_time = $_POST['end_time'];
-        // Insert the new shift into the database
-        // Add your database code here
-    }
-
     // Edit or Delete shift
-    if (isset($_POST['action']) && isset($_POST['shift_id'])) {
+    if (isset($_POST['action'])) {
         $action = $_POST['action'];
-        $shift_id = $_POST['shift_id'];
 
-        if ($action === 'edit') {
-            $updated_shift_name = $_POST['updated_shift_name'];
-            $updated_start_time = $_POST['updated_start_time'];
-            $updated_end_time = $_POST['updated_end_time'];
-            // Update the shift in the database
-            // Add your database code here
-        } elseif ($action === 'delete') {
-            // Delete the shift from the database
-            // Add your database code here
+        if ($action === 'add') {
+            $shiftName = $_POST['shift_name'];
+            $startTime = $_POST['start_time'];
+            $endTime = $_POST['end_time'];
+
+            $query = "INSERT INTO shifts (shift_name, start_time, end_time) VALUES ('$shiftName', '$startTime', '$endTime')";
+        } elseif ($action === 'edit') {
+            $id = $_POST['id'];
+            $shiftName = $_POST['shift_name'];
+            $startTime = $_POST['start_time'];
+            $endTime = $_POST['end_time'];
+
+            $query = "UPDATE shifts SET shift_name = '$shiftName', start_time = '$startTime', end_time = '$endTime' WHERE id='$id'";
+        } else {
+            $id = $_POST['id'];
+            $query = "DELETE FROM shifts WHERE id='$id'";
         }
+
+        $result = mysqli_query($db, $query) or die(mysqli_error($db));
     }
 }
 
-// Fetch shifts from the database
-// Add your database code here to retrieve the shifts
-$shifts = [
-    ['id' => 1, 'shift_name' => 'Morning', 'start_time' => '09:00:00', 'end_time' => '12:00:00'],
-    ['id' => 2, 'shift_name' => 'Afternoon', 'start_time' => '13:00:00', 'end_time' => '17:00:00'],
-    ['id' => 3, 'shift_name' => 'Evening', 'start_time' => '18:00:00', 'end_time' => '21:00:00'],
-];
+$query = "SELECT * FROM shifts";
+$result = mysqli_query($db, $query) or die(mysqli_error($db));
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Shift Management</title>
-</head>
-<body>
-    <h1>Shift Management</h1>
+<?php include 'head.php'; ?>
+<main>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Quản lý tiệc cưới</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Quản lý ca</li>
+        </ol>
+        <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Thêm ca mới
+            </div>
+            <div class="card-body">
+                <form method="POST" action="">
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputShiftName" type="text" placeholder="Ca A"  name="shift_name" required />
+                        <label for="inputShiftName">Tên ca</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputStartTime" type="time" name="start_time" required/>
+                        <label for="inputStartTime">Giờ bắt đầu</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputEndTime" type="time" name="end_time" required/>
+                        <label for="inputEndTime">Giờ kết thúc</label>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                        <button type="submit" name="action" value="add" class="btn btn-primary">Thêm</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div id="form-edit" class="card mb-4" style="display: none">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Sửa thông tin ca
+            </div>
+            <div class="card-body">
+                <form method="POST" action="">
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputShiftNameUpdate" type="text" placeholder="Ca A"  name="shift_name" required />
+                        <label for="inputShiftNameUpdate">Tên ca</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputStartTimeUpdate" type="time" name="start_time" required/>
+                        <label for="inputStartTimeUpdate">Giờ bắt đầu</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input class="form-control" id="inputEndTimeUpdate" type="time" name="end_time" required/>
+                        <label for="inputEndTimeUpdate">Giờ kết thúc</label>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                        <input id="inputIdUpdate" type="hidden" name="id"/>
+                        <button type="submit" name="action" value="edit" class="btn btn-primary">Cập nhật</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                Danh sách ca
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <tr>
+                        <th>ID</th>
+                        <th>Tên ca</th>
+                        <th>Giờ bắt đầu</th>
+                        <th>Giờ kết thúc</th>
+                        <th>Action</th>
+                    </tr>
+                    <?php while ($row = mysqli_fetch_array($result)): ?>
+                        <tr>
+                            <td><?= $row['id']; ?></td>
+                            <td><?= $row['shift_name']; ?></td>
+                            <td><?= $row['start_time']; ?></td>
+                            <td><?= $row['end_time']; ?></td>
+                            <td>
+                                <form method="POST" action="">
+                                    <input type="hidden" name="id" value="<?= $row['id']; ?>">
+                                    <button
+                                            class="btn btn-primary btn-edit"
+                                            data-id="<?= $row['id']; ?>"
+                                            data-shift-name="<?= $row['shift_name']; ?>"
+                                            data-start-time="<?= $row['start_time']; ?>"
+                                            data-end-time="<?= $row['end_time']; ?>"
+                                            type="button">Edit</button>
+                                    <button class="btn btn-danger" type="submit" name="action" value="delete">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</main>
+<?php include 'foot.php'; ?>
 
-    <h2>Add Shift</h2>
-    <form method="POST" action="">
-        <input type="text" name="shift_name" placeholder="Shift Name" required>
-        <input type="time" name="start_time" required>
-        <input type="time" name="end_time" required>
-        <button type="submit" name="add_shift">Add</button>
-    </form>
+<script>
+    $(function(){
+        $(".btn-edit").click(function(){
+            $("#inputIdUpdate").val($(this).data('id'));
+            $("#inputShiftNameUpdate").val($(this).data('shift-name'));
+            $("#inputStartTimeUpdate").val($(this).data('start-time'));
+            $("#inputEndTimeUpdate").val($(this).data('end-time'));
 
-    <h2>Edit/Delete Shift</h2>
-    <table>
-        <tr>
-            <th>Shift ID</th>
-            <th>Shift Name</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-            <th>Action</th>
-        </tr>
-        <?php foreach ($shifts as $shift) : ?>
-            <tr>
-                <td><?= $shift['id']; ?></td>
-                <td><?= $shift['shift_name']; ?></td>
-                <td><?= $shift['start_time']; ?></td>
-                <td><?= $shift['end_time']; ?></td>
-                <td>
-                    <form method="POST" action="">
-                        <input type="hidden" name="shift_id" value="<?= $shift['id']; ?>">
-                        <input type="text" name="updated_shift_name" placeholder="Updated Shift Name" required>
-                        <input type="time" name="updated_start_time" required>
-                        <input type="time" name="updated_end_time" required>
-                        <button type="submit" name="action" value="edit">Update</button>
-                        <button type="submit" name="action" value="delete">Delete</button>
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</body>
-</html>
+            $("#form-edit").css('display','block');
+        });
+    });
+</script>
